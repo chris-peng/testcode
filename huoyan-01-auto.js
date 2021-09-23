6a716e104de945779bb13111bf4b69de24081fc3
chris-peng <penghaifeng@foxmail.com>
2021-09-23 21:18:33 +0800

    Update huoyan-01-auto.js

--- huoyan-01-auto.js
+++ huoyan-01-auto.js
@@ -66,46 +66,7 @@ function interceptintercept(json, resp){
           "cheatRate": 0
         };
         var result = json.data.statisticList;
-        for(var i = 0; i < result.length; i++){
-            var row = result[i];
-
-            var active = row[activeIndex];
-
-            if(active < activeCountLimit){
-                continue;
-            }
-
-            var normalAc = row[normaAclIndex];
-            var fakeAc = row[fakeAcIndex];
-            var fackPercent = row[fackPercentIndex];
-
-            active = active - Math.floor(active * activeDeductPercent);
-            normalAc = active - fakeAc;
-            if(fakeAc != 0){
-              fackPercent = Math.round((fakeAc / active) * 10000) / 100;
-            }
-
-            if(active <= fakeAc){
-                _overview.activeNum += row[activeIndex];
-                _overview.normalNum += row[normaAclIndex];
-                continue;
-            }
-
-            row[activeIndex] = active;
-            row[normaAclIndex] = normalAc;
-            if(fakeAc != 0){
-                row[fackPercentIndex] = Math.round(fackPercent.toFixed(2) * 100) / 100;
-            }
-            _overview.activeNum += row[activeIndex];
-            _overview.normalNum += row[normaAclIndex];
-        }
-        if(_overview.activeNum > 0){
-          _overview.cheatRate += Math.round((_overview.activeNum - _overview.normalNum) / _overview.activeNum * 10000) / 100;
-        }
-        var overviewValues = document.querySelectorAll('.ant-statistic-content-value span');
-        overviewValues[0].innerText = _overview.activeNum.toLocaleString();
-        overviewValues[1].innerText = _overview.normalNum.toLocaleString();
-        overviewValues[2].innerText = _overview.cheatRate.toLocaleString();
+        
         return json;
     } else if(resp.url.indexOf('/predict/list?') >= 0){
         var activeIndex = 'activeNum';
