97696780cf92497840b0379e6b503b77f4cafa5e
chris-peng <penghaifeng@foxmail.com>
2021-09-23 13:46:57 +0800

    Create xl-01.js

---
+++ xl-01.js
@@ -0,0 +1,33 @@
+
+function interceptintercept(content, resp){
+    console.log(content, resp);
+    if(resp.config.url.indexOf('/order/page?') >= 0){
+      var json = JSON.parse(content);
+      var data = json.result.pageResult;
+      for(var i = 0; i < data.length; i++){
+        var row = data[i];
+        if(row.column10 == '结算数据'){
+          if(row.column07 == '3750'){
+            row.column07 == '3688';
+            row.column12 == '147520';
+          }
+        }
+      }
+      return JSON.stringify(json);
+    }
+    return content;
+}
+
+var hookscript = document.createElement('script');
+hookscript.src='https://unpkg.com/ajax-hook@2.0.3/dist/ajaxhook.min.js';
+document.head.appendChild(hookscript);
+
+function runrun(){
+    alert('OK！');
+    ah.proxy({
+      onResponse: function(response, handler){
+        response.response = interceptintercept(response.response, response);
+        handler.next(response);
+      }
+    });
+}
