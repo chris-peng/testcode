cbaeb3f327d2d825f96a96c5aa62f501f69f8b24
chris-peng <penghaifeng@foxmail.com>
2022-06-12 19:08:27 +0800

    Update jd-js.js

--- yicixing/jd-js.js
+++ yicixing/jd-js.js
@@ -1,29 +1,16 @@
 function interceptintercept(content, resp){
     console.log(content, resp);
     if(resp.url.indexOf('/statement/list?') >= 0){
-      alert(111);
       var json = JSON.parse(content);
-      var data = json.result.pageResult.page;
+      var data = json.data.result;
       for(var i = 0; i < data.length; i++){
         var row = data[i];
-        if(row.column10 == '12月结算'){
-          if(row.linkKey == '7275' && row.column07 == '169'){
-            row.column07 = '149';
-            row.column12 = '5960';
-          }
-          if(row.linkKey == '6693' && row.column07 == '321'){
-            row.column08 = '15.5934579439';
-            row.column12 = '5005.5';
-          }
-          if(row.linkKey == '6694' && row.column07 == '2343'){
-            row.column07 = '2253';
-            row.column12 = '90120';
-          }
-          if(row.linkKey == '7211' && row.column07 == '2573'){
-            row.column07 = '2403';
-            row.column12 = '96120';
-          }
-        }
+        row.activeNum = 67146;
+        row.normalNum = 63629;
+        row.cheatNum = 3517;
+        row.cheatRate = 5.24;
+        row.statementCost = 460572.08;
+        row.avgActivePrice = 7.24;
       }
       console.log('json', json);
       return JSON.stringify(json);
