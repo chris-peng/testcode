63ec294aaa3383d995c329974c4c0653b3772eaa
chris-peng <penghaifeng@foxmail.com>
2022-06-12 19:17:40 +0800

    Update intercept-example.js

--- intercept-example.js
+++ intercept-example.js
@@ -38,7 +38,7 @@ fetch = function (url, options={}) {
                 newRes.text=function(){
                 return new Promise((resolvej,rejectj)=>{
                     oldtextc.call(this).then((j)=>{
-                        resolvej.call(this,JSON.stringify(interceptintercept(j, this)));
+                        resolvej.call(this,interceptintercept(j, this));
                     })
                 .catch(errj=>{rejectj(errj)})})};
                 return newRes;
