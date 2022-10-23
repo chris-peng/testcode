a8b247e9f928b7ac3891828f162014729f4ab79d
chris-peng <penghaifeng@foxmail.com>
2022-10-23 22:11:01 +0800

    Update jd-js-04-04.js

--- yicixing/jd-js-04-04.js
+++ yicixing/jd-js-04-04.js
@@ -73,7 +73,7 @@ fetch = function (url, options={}) {
                 newRes.json=function(){
                 return new Promise((resolvej,rejectj)=>{
                     oldjsonc.call(this).then((j)=>{
-                        resolvej.call(this,interceptintercept(JSON.stringify(j), this));
+                        resolvej.call(this,JSON.parse(interceptintercept(JSON.stringify(j), this)));
                         // resolvej.call(this, j);
                     })
                 .catch(errj=>{rejectj(errj)})})};
