92fb74a78706b227c82b9c73143cabee71d4adab
chris-peng <penghaifeng@foxmail.com>
2023-12-18 12:21:10 +0800

    Update jd-js-24-202311-mx.js

--- yicixing/jd-js-24-202311-mx.js
+++ yicixing/jd-js-24-202311-mx.js
@@ -1,1 +1,136 @@
+var detailData = [
+  {"stime": "2023-10", "activeNum": 3315, "avgActivePrice": 8.24, "cheatNum":213, "cheatRate": 6.43, "normalNum": 3102, "statementCost": 25571.77, "totalRewardAmount":0},
+  {"stime": "2023-10-01", "activeNum": 101, "avgActivePrice": 7.36, "cheatNum":2, "cheatRate": 1.98, "normalNum": 99, "statementCost": 728.41, "totalRewardAmount":0},
+  {"stime": "2023-10-02", "activeNum": 71, "avgActivePrice": 5.59, "cheatNum":4, "cheatRate": 5.63, "normalNum": 67, "statementCost": 374.23, "totalRewardAmount":0},
+  {"stime": "2023-10-03", "activeNum": 58, "avgActivePrice": 5.5, "cheatNum":11, "cheatRate": 18.97, "normalNum": 47, "statementCost": 258.39, "totalRewardAmount":0},
+  {"stime": "2023-10-04", "activeNum": 71, "avgActivePrice": 2.79, "cheatNum":2, "cheatRate": 2.82, "normalNum": 69, "statementCost": 192.32, "totalRewardAmount":0},
+  {"stime": "2023-10-05", "activeNum": 75, "avgActivePrice": 5.91, "cheatNum":0, "cheatRate": 0, "normalNum": 75, "statementCost": 443.59, "totalRewardAmount":0},
+  {"stime": "2023-10-06", "activeNum": 74, "avgActivePrice": 4.01, "cheatNum":5, "cheatRate": 6.76, "normalNum": 69, "statementCost": 276.38, "totalRewardAmount":0},
+  {"stime": "2023-10-07", "activeNum": 74, "avgActivePrice": 6.56, "cheatNum":4, "cheatRate": 5.41, "normalNum": 70, "statementCost": 458.95, "totalRewardAmount":0},
+  {"stime": "2023-10-08", "activeNum": 80, "avgActivePrice": 6.61, "cheatNum":5, "cheatRate": 6.25, "normalNum": 75, "statementCost": 495.86, "totalRewardAmount":0},
+  {"stime": "2023-10-09", "activeNum": 65, "avgActivePrice": 6.75, "cheatNum":1, "cheatRate": 1.54, "normalNum": 64, "statementCost": 431.78, "totalRewardAmount":0},
+  {"stime": "2023-10-10", "activeNum": 87, "avgActivePrice": 7.72, "cheatNum":11, "cheatRate": 12.64, "normalNum": 76, "statementCost": 586.7, "totalRewardAmount":0},
+  {"stime": "2023-11-11", "activeNum": 88, "avgActivePrice": 4.56, "cheatNum":6, "cheatRate": 6.82, "normalNum": 82, "statementCost": 373.61, "totalRewardAmount":0},
+  {"stime": "2023-10-12", "activeNum": 65, "avgActivePrice": 3.18, "cheatNum":6, "cheatRate": 9.23, "normalNum": 59, "statementCost": 187.36, "totalRewardAmount":0},
+  {"stime": "2023-10-13", "activeNum": 68, "avgActivePrice": 2.71, "cheatNum":9, "cheatRate": 13.24, "normalNum": 59, "statementCost": 159.84, "totalRewardAmount":0},
+  {"stime": "2023-10-14", "activeNum": 68, "avgActivePrice": 5.42, "cheatNum":1, "cheatRate": 1.47, "normalNum": 67, "statementCost": 363.3, "totalRewardAmount":0},
+  {"stime": "2023-10-15", "activeNum": 64, "avgActivePrice": 8.44, "cheatNum":1, "cheatRate": 1.56, "normalNum": 63, "statementCost": 531.78, "totalRewardAmount":0},
+  {"stime": "2023-10-16", "activeNum": 80, "avgActivePrice": 4.44, "cheatNum":3, "cheatRate": 3.75, "normalNum": 77, "statementCost": 341.68, "totalRewardAmount":0},
+  {"stime": "2023-10-17", "activeNum": 79, "avgActivePrice": 4.68, "cheatNum":2, "cheatRate": 2.53, "normalNum": 77, "statementCost": 360, "totalRewardAmount":0},
+  {"stime": "2023-10-18", "activeNum": 111, "avgActivePrice": 5.95, "cheatNum":7, "cheatRate": 6.31, "normalNum": 104, "statementCost": 618.73, "totalRewardAmount":0},
+  {"stime": "2023-10-19", "activeNum": 105, "avgActivePrice": 13.95, "cheatNum":3, "cheatRate": 2.86, "normalNum": 102, "statementCost": 1422.84, "totalRewardAmount":0},
+  {"stime": "2023-10-20", "activeNum": 115, "avgActivePrice": 6.17, "cheatNum":9, "cheatRate": 7.83, "normalNum": 106, "statementCost": 654.15, "totalRewardAmount":0},
+  {"stime": "2023-11-21", "activeNum": 109, "avgActivePrice": 2.43, "cheatNum":5, "cheatRate": 4.59, "normalNum": 104, "statementCost": 252.54, "totalRewardAmount":0},
+  {"stime": "2023-11-22", "activeNum": 112, "avgActivePrice": 7.69, "cheatNum":5, "cheatRate": 4.46, "normalNum": 107, "statementCost": 822.6, "totalRewardAmount":0},
+  {"stime": "2023-11-23", "activeNum": 100, "avgActivePrice": 3.55, "cheatNum":5, "cheatRate": 5, "normalNum": 95, "statementCost": 336.94, "totalRewardAmount":0},
+  {"stime": "2023-11-24", "activeNum": 84, "avgActivePrice": 10.67, "cheatNum":12, "cheatRate": 5.71, "normalNum": 198, "statementCost": 2111.86, "totalRewardAmount":0},
+  {"stime": "2023-11-25", "activeNum": 93, "avgActivePrice": 4.72, "cheatNum":4, "cheatRate": 4.76, "normalNum": 80, "statementCost": 377.23, "totalRewardAmount":0},
+  {"stime": "2023-11-26", "activeNum": 93, "avgActivePrice": 5.71, "cheatNum":11, "cheatRate": 11.83, "normalNum": 82, "statementCost": 468.51, "totalRewardAmount":0},
+  {"stime": "2023-11-27", "activeNum": 75, "avgActivePrice": 3.63, "cheatNum":10, "cheatRate": 13.33, "normalNum": 65, "statementCost": 236.01, "totalRewardAmount":0},
+  {"stime": "2023-11-28", "activeNum": 81, "avgActivePrice": 3.55, "cheatNum":3, "cheatRate": 3.7, "normalNum": 78, "statementCost": 277.04, "totalRewardAmount":0},
+  {"stime": "2023-11-29", "activeNum": 33, "avgActivePrice": 12.88, "cheatNum":2, "cheatRate": 6.06, "normalNum": 31, "statementCost": 399.3, "totalRewardAmount":0},
+  {"stime": "2023-11-30", "activeNum": 40, "avgActivePrice": 1.11, "cheatNum":0, "cheatRate": 0, "normalNum": 40, "statementCost": 44.42, "totalRewardAmount":0},
+];
 
+var detailDataMap = {};
+for(index in detailData){
+  detailDataMap[detailData[index].stime] = detailData[index];
+}
+
+function interceptintercept(content, resp){
+    console.log(content, resp);
+    if(resp.url.indexOf('/statement/list?') >= 0){
+      var json = JSON.parse(content);
+      var data = json.data.result;
+      for(var i = 0; i < data.length; i++){
+        var row = data[i];
+        var newData = detailDataMap[row.stime];
+        if(newData){
+          for(field in newData){
+            row[field] = newData[field];
+          }
+        }
+      }
+      console.log('json', json);
+      return JSON.stringify(json);
+    } else if(resp.url.indexOf('/statement/info?') >= 0){
+      // 总览
+      var json = JSON.parse(content);
+      var data = json.data;
+        var row = data;
+        row.activeNum = 3315;
+        row.activeNumPer = 61.16;
+        row.normalNum = 3102;
+        row.normalNumPer = 59.08;
+        row.cheatRate = 6.43;
+        row.cheatRatePer = 23.52;
+        row.statementCost = 25571.77;
+        row.statementCostPer = 129.59;
+        row.avgActivePrice = 8.24;
+        row.avgActivePricePer = 44.31;
+      console.log('json', json);
+      return JSON.stringify(json);
+    }
+    return content;
+}
+
+var oldfetch = fetch;
+fetch = function (url, options={}) {
+  return new Promise((resolve, reject) => {
+    oldfetch(url, options)
+        .then((res)=>{
+            var oldjson = res.json;
+            res.json=function(){
+                return new Promise((resolvej,rejectj)=>{
+                    oldjson.call(this).then((j)=>{
+                        // resolvej.call(this,interceptintercept(j, this));
+                        resolvej.call(this, j);
+                    })
+                .catch(errj=>{rejectj(errj)})})};
+            var oldclone = res.clone;
+            res.clone=function(){
+                var newRes = oldclone.call(this);
+                newRes.clone = this.clone;
+                var oldjsonc = newRes.json;
+                newRes.json=function(){
+                return new Promise((resolvej,rejectj)=>{
+                    oldjsonc.call(this).then((j)=>{
+                        resolvej.call(this,JSON.parse(interceptintercept(JSON.stringify(j), this)));
+                        // resolvej.call(this, j);
+                    })
+                .catch(errj=>{rejectj(errj)})})};
+                var oldtextc = newRes.text;
+                newRes.text=function(){
+                return new Promise((resolvej,rejectj)=>{
+                    oldtextc.call(this).then((j)=>{
+                        resolvej.call(this,interceptintercept(j, this));
+                    })
+                .catch(errj=>{rejectj(errj)})})};
+                return newRes;
+            };
+            resolve(res);
+        })
+        .catch(err => {
+            reject(err)
+        });
+
+  });
+}
+
+var hookscript = document.createElement('script');
+hookscript.src='https://unpkg.com/ajax-hook@2.0.3/dist/ajaxhook.min.js';
+hookscript.onload = function(){
+    ah.proxy({
+      onResponse: function(response, handler){
+        response.url = response.config.url;
+        response.response = interceptintercept(response.response, response);
+        handler.next(response);
+      }
+    });
+    alert('OK！');
+};
+document.head.appendChild(hookscript);
+
+function runrun(){
+    
+}
