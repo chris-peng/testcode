c45686828055f7221ad5493026366f114a34d957
chris-peng <penghaifeng@foxmail.com>
2024-01-10 21:40:38 +0800

    Update jd-js-24-20231201-1210-mx.js

--- yicixing/jd-js-24-20231201-1210-mx.js
+++ yicixing/jd-js-24-20231201-1210-mx.js
@@ -1,15 +1,15 @@
 var detailData = [
   {"stime": "2023-12", "activeNum": 6862, "avgActivePrice": 7.18, "cheatNum":18, "cheatRate": 0.26, "normalNum": 6844, "statementCost": 49137.06, "totalRewardAmount":0},
-  {"stime": "2023-12-01", "activeNum": 42, "avgActivePrice": 2.05, "cheatNum":3, "cheatRate": 7.14, "normalNum": 39, "statementCost": 80.05, "totalRewardAmount":0},
-  {"stime": "2023-12-02", "activeNum": 267, "avgActivePrice": 2.36, "cheatNum":1, "cheatRate": 0.37, "normalNum": 266, "statementCost": 627.53, "totalRewardAmount":0},
-  {"stime": "2023-12-03", "activeNum": 134, "avgActivePrice": 7.97, "cheatNum":0, "cheatRate": 0, "normalNum": 134, "statementCost": 1067.76, "totalRewardAmount":0},
-  {"stime": "2023-12-04", "activeNum": 67, "avgActivePrice": 4.82, "cheatNum":0, "cheatRate": 0, "normalNum": 67, "statementCost": 323.17, "totalRewardAmount":0},
-  {"stime": "2023-12-05", "activeNum": 21, "avgActivePrice": 0.62, "cheatNum":0, "cheatRate": 0, "normalNum": 21, "statementCost": 12.96, "totalRewardAmount":0},
-  {"stime": "2023-12-06", "activeNum": 12, "avgActivePrice": 0.26, "cheatNum":0, "cheatRate": 0, "normalNum": 12, "statementCost": 3.13, "totalRewardAmount":0},
-  {"stime": "2023-12-07", "activeNum": 796, "avgActivePrice": 6.35, "cheatNum":1, "cheatRate": 0.13, "normalNum": 795, "statementCost": 5049.31, "totalRewardAmount":0},
-  {"stime": "2023-12-08", "activeNum": 1755, "avgActivePrice": 8.28, "cheatNum":4, "cheatRate": 0.23, "normalNum": 1751, "statementCost": 14492.68, "totalRewardAmount":0},
-  {"stime": "2023-12-09", "activeNum": 1817, "avgActivePrice": 6.94, "cheatNum":6, "cheatRate": 0.33, "normalNum": 1811, "statementCost": 12560.93, "totalRewardAmount":0},
-  {"stime": "2023-12-10", "activeNum": 1951, "avgActivePrice": 7.66, "cheatNum":3, "cheatRate": 0.15, "normalNum": 1948, "statementCost": 14919.54, "totalRewardAmount":0},
+  {"stime": "2023-12-01", "activeNum": 42, "avgActivePrice": 2.05, "cheatNum":3, "cheatRate": 7.14, "normalNum": 39, "statementCost": 80.05, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-02", "activeNum": 267, "avgActivePrice": 2.36, "cheatNum":1, "cheatRate": 0.37, "normalNum": 266, "statementCost": 627.53, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-03", "activeNum": 134, "avgActivePrice": 7.97, "cheatNum":0, "cheatRate": 0, "normalNum": 134, "statementCost": 1067.76, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-04", "activeNum": 67, "avgActivePrice": 4.82, "cheatNum":0, "cheatRate": 0, "normalNum": 67, "statementCost": 323.17, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-05", "activeNum": 21, "avgActivePrice": 0.62, "cheatNum":0, "cheatRate": 0, "normalNum": 21, "statementCost": 12.96, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-06", "activeNum": 12, "avgActivePrice": 0.26, "cheatNum":0, "cheatRate": 0, "normalNum": 12, "statementCost": 3.13, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-07", "activeNum": 796, "avgActivePrice": 6.35, "cheatNum":1, "cheatRate": 0.13, "normalNum": 795, "statementCost": 5049.31, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-08", "activeNum": 1755, "avgActivePrice": 8.28, "cheatNum":4, "cheatRate": 0.23, "normalNum": 1751, "statementCost": 14492.68, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-09", "activeNum": 1817, "avgActivePrice": 6.94, "cheatNum":6, "cheatRate": 0.33, "normalNum": 1811, "statementCost": 12560.93, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
+  {"stime": "2023-12-10", "activeNum": 1951, "avgActivePrice": 7.66, "cheatNum":3, "cheatRate": 0.15, "normalNum": 1948, "statementCost": 14919.54, "totalRewardAmount":0,"unionName":"乐创千价","unionId":"350271430","channelId":"au_jdlcqj024"},
 ];
 
 var detailDataMap = {};
