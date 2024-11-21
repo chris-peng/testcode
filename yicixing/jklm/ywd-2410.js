3ee80ac79e6c53164b5e176d27836b381beb66f4
chris-peng <penghaifeng@foxmail.com>
2024-11-21 16:57:24 +0800

    Update ywd-2410.js

--- yicixing/jklm/ywd-2410.js
+++ yicixing/jklm/ywd-2410.js
@@ -1,8 +1,8 @@
 var detailData = [
   {"channelPackageName": "lechuang_android_lechuang16","month": "2024-09","newUserNum": 445,"cheatUserNum": 100,"payUserNum": 345,"price": 12.43,"amount": 4289},
   {"channelPackageName": "lechuang_android_lechuang15","month": "2024-09","newUserNum": 35489,"cheatUserNum": 9304,"payUserNum": 26185,"price": 11.03,"amount": 288854},
-  {"channelPackageName": "lechuang_android_lechuang10","month": "2024-09","newUserNum": 457,"cheatUserNum": 82,"payUserNum": 375,"price": 14.2,"amount": 5324},
-  {"channelPackageName": "lechuang_android_lechuang08","month": "2024-09","newUserNum": 904,"cheatUserNum": 328,"payUserNum": 576,"price": 6.59,"amount": 3796},
+  {"channelPackageName": "lechuang_android_lechuang10","month": "2024-10","newUserNum": 457,"cheatUserNum": 82,"payUserNum": 375,"price": 14.2,"amount": 5324},
+  {"channelPackageName": "lechuang_android_lechuang08","month": "2024-10","newUserNum": 17150,"cheatUserNum": 6451,"payUserNum": 10699,"price": 8.79,"amount": 94045},
   {"channelPackageName": "lechuang_android_lechuang06","month": "2024-09","newUserNum": 516,"cheatUserNum": 80,"payUserNum": 436,"price": 12.2,"amount": 5317},
   {"channelPackageName": "lechuang_android_lechuang05","month": "2024-09","newUserNum": 608,"cheatUserNum": 177,"payUserNum": 431,"price": 7.62,"amount": 3285},
   {"channelPackageName": "lechuang_android_lechuang03","month": "2024-09","newUserNum": 505,"cheatUserNum": 101,"payUserNum": 404,"price": 13.5,"amount": 5455},
