8d2b640ccae7a9b1cee61442c8de6c81256a99a3
chris-peng <penghaifeng@foxmail.com>
2025-04-02 14:22:59 +0800

    Update rwd-lechuang08.js

--- yicixing/jklm/rwd-lechuang08.js
+++ yicixing/jklm/rwd-lechuang08.js
@@ -33,6 +33,7 @@ var detailData = [
   {"channelPackageName": "lechuang_android_lechuang08","day": "2025-03-28","users0": 122,"cheatUsers": 6,"cheatUserRatio":0.05,"users": 116,"price": 8.64,"amount": 1002.24},
   {"channelPackageName": "lechuang_android_lechuang08","day": "2025-03-29","users0": 119,"cheatUsers": 11,"cheatUserRatio":0.09,"users": 108,"price": 6,"amount": 648},
   {"channelPackageName": "lechuang_android_lechuang08","day": "2025-03-30","users0": 116,"cheatUsers": 2,"cheatUserRatio":0.02,"users": 114,"price": 6,"amount": 684},
+  {"channelPackageName": "lechuang_android_lechuang08","day": "2025-03-31","users0": 123,"cheatUsers": 17,"cheatUserRatio":0.14,"users": 106,"price": 22.16,"amount": 2348.96},
 ];
 
 var detailDataMap = {};
